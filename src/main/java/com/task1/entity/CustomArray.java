008cf18bf2762575151b671ecf294f2d0299c898
VadimArtish <123artisch.vadim123262003@mail.ru>
2022-02-21 19:00:22 +0300

    change test

--- src/main/java/com/task1/entity/CustomArray.java
+++ src/main/java/com/task1/entity/CustomArray.java
@@ -1,11 +1,16 @@
 package com.task1.entity;
 
 import com.task1.exception.AppException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
 
 public class CustomArray {
+
     private int[] array;
+    private static final Logger LOGGER = LogManager.getLogger(CustomArray.class);
 
     public CustomArray() {
     }
@@ -20,6 +25,7 @@ public class CustomArray {
 
     public void setArray(int... array) throws AppException {
         if (array == null) {
+            LOGGER.log(Level.ERROR, "Null pointer!");
             throw new AppException("Null pointer!");
         }
         this.array = array;
@@ -27,6 +33,7 @@ public class CustomArray {
 
     public int getElement(int index) throws AppException {
         if (index < 0 || index >= array.length) {
+            LOGGER.log(Level.ERROR, "Index out of range!");
             throw new AppException("Index out of range!");
         }
 
@@ -35,6 +42,7 @@ public class CustomArray {
 
     public void setElement(int index, int element) throws AppException {
         if (index < 0 || index >= array.length) {
+            LOGGER.log(Level.ERROR, "Index out of range!");
             throw new AppException("Index out of range!");
         }
 
